8ce7e5dd6805da921ae0a8f6342dee3f7bfb8cbb
dndbdchhf <adnansamirimon1@gmail.com>
2026-08-07 03:40:21 +0300

    Update autoreactseen.js

--- scripts/events/autoreactseen.js
+++ scripts/events/autoreactseen.js
@@ -1,1 +1,35 @@
+module.exports = {
+	config: {
+		name: "autoreactseen",
+		version: "1.0",
+		author: "ADNAN",
+		category: "events"
+	},
 
+	onStart: async ({ event, api }) => {
+		try {
+			if (!event.messageID)
+				return;
+
+			const emojis = [
+				"👍", "❤️", "😆", "😮", "😢", "😡",
+				"🔥", "🥰", "😍", "🤩", "🎉", "💯",
+				"😁", "😎", "🤣", "👏", "✨", "💖"
+			];
+
+			const randomEmoji = emojis[Math.floor(Math.random() * emojis.length)];
+
+			api.markAsRead(event.threadID);
+
+			api.setMessageReaction(
+				randomEmoji,
+				event.messageID,
+				() => {},
+				true
+			);
+
+		} catch (err) {
+			console.log(err);
+		}
+	}
+};
